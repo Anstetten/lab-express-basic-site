8278e635f425d0629772fcebf395d4b01e4bc14f
Zsolt HORVATH <h.zsola@gmail.com>
2021-07-21 16:32:37 +0200

    Add grid, and dummy data. Make grid responsive. Styling of book cards

---
+++ main.js
@@ -0,0 +1,69 @@
+let books = [
+    {
+        Title: "HArry Potter",
+        Year: 1998,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter2",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter3",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter4",
+        Year: 2008,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter5",
+        Year: 2010,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter2",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter3",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter4",
+        Year: 2008,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter5",
+        Year: 2010,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter2",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter3",
+        Year: 1999,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter4",
+        Year: 2008,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+    {
+        Title: "HArry Potter5",
+        Year: 2010,
+        Cover: "https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg"
+    },
+]
+
+module.exports = books;
--- routes/works.js
+++ routes/works.js
@@ -1,9 +1,13 @@
+
 const express=require('express');
 const router=express.Router();
 
+const books = require('../main')
+
 router.get("/works", (req, res,next) =>{
-    res.render('works.hbs',{
-        css: ["works.css"]
+    res.render('works.hbs',{        
+        css: ["works.css"],
+        books,
     });
 });
 
